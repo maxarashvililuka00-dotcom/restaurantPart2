91a5cb3b058b161b7c63941724bcb6b9f42ed676
luka-creator <maxarashvililuka00@gmail.com>
2026-05-01 22:36:22 +0400

    add animations, dark mode fix, back to top, translate

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Theme } from './theme';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('restaurantPart2');
+  constructor(public theme: Theme) {}
 }
